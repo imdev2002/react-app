7b699e44bc14e8685a1eebf37767984a3404d5e0
mxz <60683892+mxzxm@users.noreply.github.com>
2023-03-17 19:50:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,148 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { Howl } from 'howler';
+import './App.css';
 import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
 
 function App() {
-	const [socket, setSocket] = useState(null);
-	const [audioData, setAudioData] = useState([]);
-	const [bufferPositions, setBufferPositions] = useState([]);
-	const [currentPosition, setCurrentPosition] = useState(0);
-	const [sound, setSound] = useState(null);
+	const [audioUrl, setAudioUrl] = useState(null);
+	const [audioChunks, setAudioChunks] = useState([]);
+	const [infoTrack, setInfoTrack] = useState('');
 
 	useEffect(() => {
-		const socket = io('http://localhost:3000/track');
-		setSocket(socket);
-		socket.emit('play', { trackId: '6411add8403a31b106d404a9' });
-		socket.on('message', ({ data, position }) => {
-			setAudioData((audioData) => [...audioData, data]);
-			setBufferPositions((bufferPositions) => [
-				...bufferPositions,
-				position,
-			]);
-		});
-
-		socket.on('end', () => {
-			socket.close();
-			setCurrentPosition(0);
+		const socket = new io('ws://localhost:3000/track');
+		socket.emit('play', { trackId: '6411add9f7a8fc311762d5db' });
+		socket.on('message', (data) => {
+			setAudioChunks((chunks) => [...chunks, data.data]);
 		});
 
 		return () => {
-			if (socket) {
-				socket.close();
-			}
+			socket.close();
+		};
+	}, []);
+	useEffect(() => {
+		const socket = new io('ws://localhost:3000/track');
+		socket.emit('info', { trackId: '6411add9f7a8fc311762d5db' });
+		socket.on('message', (data) => {
+			setInfoTrack(data);
+		});
+		return () => {
+			socket.close();
 		};
 	}, []);
 
 	useEffect(() => {
-		if (audioData.length > 0) {
-			setSound(
-				new Howl({
-					src: [],
-					format: ['mp3'],
-					onload: () => {
-						setSound((sound) => {
-							if (sound) {
-								sound.play();
-							}
-							return sound;
-						});
-					},
-				}),
-			);
+		if (audioChunks.length > 0) {
+			const blob = new Blob(audioChunks, { type: 'audio/mpeg' });
+			const audioUrl = URL.createObjectURL(blob);
+			setAudioUrl(audioUrl);
 		}
-	}, [audioData]);
-
-	useEffect(() => {
-		if (sound && bufferPositions.length > 0) {
-			const position =
-				bufferPositions.find((pos) => pos > currentPosition) ||
-				bufferPositions[0];
-			const index = bufferPositions.indexOf(position);
-			const buffer = audioData[index];
-			const blob = new Blob([buffer], { type: 'audio/mp3' });
-
-			setSound((sound) => {
-				sound.unload();
-				sound._src = URL.createObjectURL(blob);
-				sound.load();
-				sound.seek(position - currentPosition);
-				sound.play();
-				return sound;
-			});
-
-			setCurrentPosition(position);
-		}
-	}, [sound, bufferPositions, currentPosition, audioData]);
-
-	const handlePlay = () => {
-		if (sound) {
-			sound.play();
-		}
-	};
-
-	const handlePause = () => {
-		if (sound) {
-			sound.pause();
-		}
-	};
-
-	const handleStop = () => {
-		if (sound) {
-			sound.stop();
-			setCurrentPosition(0);
-		}
-	};
+	}, [audioChunks]);
 
 	return (
 		<div>
-			<button onClick={handlePlay}>Play</button>
-			<button onClick={handlePause}>Pause</button>
-			<button onClick={handleStop}>Stop</button>
+			{infoTrack && <h4>{infoTrack}</h4>}
+			{audioUrl && (
+				<audio
+					controls
+					autoPlay="true"
+					src={audioUrl}
+				></audio>
+			)}
 		</div>
 	);
 }
-
 export default App;
-// import './App.css';
-// import io from 'socket.io-client';
-// import { useEffect, useState } from 'react';
-
-// function App() {
-// 	const [audioUrl, setAudioUrl] = useState(null);
-// 	const [audioChunks, setAudioChunks] = useState([]);
-
-// 	useEffect(() => {
-// 		const socket = new io('ws://localhost:3000/track');
-// 		socket.emit('play', { trackId: '6411add8403a31b106d404a9' });
-// 		socket.on('message', (data) => {
-// 			setAudioChunks((chunks) => [...chunks, data]);
-// 		});
-
-// 		return () => {
-// 			socket.close();
-// 		};
-// 	}, []);
-
-// 	useEffect(() => {
-// 		if (audioChunks.length > 0) {
-// 			const blob = new Blob(audioChunks, { type: 'audio/mpeg' });
-// 			const audioUrl = URL.createObjectURL(blob);
-// 			setAudioUrl(audioUrl);
-// 		}
-// 	}, [audioChunks]);
-
-// 	return (
-// 		<div>
-// 			{audioUrl && (
-// 				<audio controls>
-// 					<source
-// 						src={audioUrl}
-// 						type="audio/mpeg"
-// 					/>
-// 				</audio>
-// 			)}
-// 		</div>
-// 	);
-// }
-
-// export default App;
