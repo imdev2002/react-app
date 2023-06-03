bbc5e0dc1b16e520dc863b80e546a41cdaca35c7
Kwzng <60683892+Kwzng@users.noreply.github.com>
2023-06-03 19:26:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,50 +3,13 @@ import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
 function App() {
-	const [audioUrl, setAudioUrl] = useState(null);
-	const [audioChunks, setAudioChunks] = useState([]);
-	const [infoTrack, setInfoTrack] = useState('');
-
-	useEffect(() => {
-		const socket = new io('ws://localhost:3000/track');
-		socket.emit('play', { trackId: '6411add9f7a8fc311762d5db' });
-		socket.on('message', (data) => {
-			setAudioChunks((chunks) => [...chunks, data.data]);
-		});
-
-		return () => {
-			socket.close();
-		};
-	}, []);
-	useEffect(() => {
-		const socket = new io('ws://localhost:3000/track');
-		socket.emit('info', { trackId: '6411add9f7a8fc311762d5db' });
-		socket.on('message', (data) => {
-			setInfoTrack(data);
-		});
-		return () => {
-			socket.close();
-		};
-	}, []);
-
-	useEffect(() => {
-		if (audioChunks.length > 0) {
-			const blob = new Blob(audioChunks, { type: 'audio/mpeg' });
-			const audioUrl = URL.createObjectURL(blob);
-			setAudioUrl(audioUrl);
-		}
-	}, [audioChunks]);
-
 	return (
 		<div>
-			{infoTrack && <h4>{infoTrack}</h4>}
-			{audioUrl && (
-				<audio
-					controls
-					autoPlay="true"
-					src={audioUrl}
-				></audio>
-			)}
+			<audio
+				controls
+				autoPlay="true"
+				src="http://localhost:3000/file/d91bfcefa173b9df7a136c0d2fdda998"
+			></audio>
 		</div>
 	);
 }
