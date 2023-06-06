24c5c16c4f92175639e3e36d52548040b54483ca
Kwzng <60683892+Kwzng@users.noreply.github.com>
2023-06-06 16:32:26 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
 function App() {
+	const socket = new io('ws://localhost:3000/notification');
+	socket.on("notification", (data) => {
+		console.log(data);
+	});
 	return (
 		<div>
 			<audio
 				controls
 				autoPlay="true"
-				src="http://localhost:3000/file/d91bfcefa173b9df7a136c0d2fdda998"
+			// src="http://localhost:3000/file/d91bfcefa173b9df7a136c0d2fdda998"
 			></audio>
 		</div>
 	);
